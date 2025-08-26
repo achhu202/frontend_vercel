7008677d9e4ccd822e2b7339c5dc6086c07b3b7f
achhu202 <sathy202@gmail.com>
2025-08-26 12:26:51 +0530

    Update API's.js

--- react_redux/src/API's.js
+++ react_redux/src/API's.js
@@ -1,2 +1,7 @@
-export const registerAPI = "http://localhost:5000/api/auth/register";
-export const loginAPI = "http://localhost:5000/api/auth/login";
+const BASE_URL =
+  import.meta.env.MODE === "development"
+    ? "http://localhost:5000"
+    : "https://deploy-backend-1-opk3.onrender.com/";
+
+export const registerAPI = `${BASE_URL}/api/auth/register`;
+export const loginAPI = `${BASE_URL}/api/auth/login`;
